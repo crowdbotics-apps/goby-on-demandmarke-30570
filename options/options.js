af5eab05b6c6c7aec4c06eb8b3fd19fb4849a8d4
Crowdbotics <team@crowdbotics.com>
2021-09-15 09:28:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "goby_on_demandmarke_30570Identifier",
+  url: "https://goby_on_demandmarke_30570Identifier.botics.co",
+  api: "https://goby_on_demandmarke_30570Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
